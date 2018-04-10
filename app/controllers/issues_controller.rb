3f088236538a4bc60c99ed72d6badbc22e4c6a7f
sousousore1 <souichi.saitou@gmail.com>
2018-04-10 18:29:05 +0900

    Add comments

--- app/controllers/issues_controller.rb
+++ app/controllers/issues_controller.rb
@@ -3,5 +3,6 @@ class IssuesController < ApplicationController
 
   def show
     @issue = @github_client.issue(@current_repository.full_name, params[:id])
+    @comments = @github_client.issue_comments(@current_repository.full_name, params[:id])
   end
 end
